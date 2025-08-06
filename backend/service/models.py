75f54ea74a1aa56c7008e5b1f5f30cb4ea0c951b
Arash-PC <92019869+ArashNS@users.noreply.github.com>
2025-08-06 12:30:41 +0330

    Update models.py

--- backend/service/models.py
+++ backend/service/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     # https://docs.djangoproject.com/en/5.2/ref/models/fields/#django.db.models.Field.choices
     ROLE_CHOICES = (
-        ('client'),
-        ('manager'),
+        ('client', 'Client'),
+        ('manager', 'Manager')
     )
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='client')
